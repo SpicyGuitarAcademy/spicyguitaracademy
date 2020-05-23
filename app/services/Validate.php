726b5c2dd41551db83b90f2317bc68f8c79a75e3
ebuka <ebukaodini@gmail.com>
2020-05-23 14:20:01 +0100

    added the validate service

--- app/services/Validate.php
+++ app/services/Validate.php
@@ -5,126 +5,176 @@ class Validate
 {
 
    private $field = "";
+   private $value = "";
+   private $errmsg = "";
    private $errors = [];
 
-   // numbers
-   public function numbers(string $field, $value, string $errmsg = "invalid number!")
+   // any
+   public function any(string $field, string $value, string $errmsg = "invalid field!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
+      return $this;
+   }
+
+   // numbers
+   public function numbers(string $field, string $value, string $errmsg = "invalid number!")
+   {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_INT, [FILTER_FLAG_ALLOW_OCTAL, FILTER_FLAG_ALLOW_HEX]) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // amount
-   public function amount(string $field, $value, string $errmsg = "invalid amount!")
+   public function amount(string $field, string $value, string $errmsg = "invalid amount!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_FLOAT, [FILTER_FLAG_ALLOW_THOUSAND]) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
-
-   
-   private static $any_param_type = "/[0-9A-Za-z -_]+/";
-
-   private static $digits_param_type  = "/[0-9]+/";                          //:d
-
-   private static $letters_param_type = "/[A-Za-z]+/";                       //:a
-
-   private static $letters_plus_param_type = "/[A-Za-z -_]+/";               //:a+
-
-   private static $alpha_numeric_param_type = "/[0-9A-Za-z]+/";              //:x
-
-   private static $alpha_numeric_plus_param_type = "/[0-9A-Za-z -_]+/";      //:x+
-
    // letters
-   public function letters(string $field, $value, string $errmsg = "invalid letters!")
+   public function letters(string $field, string $value, string $errmsg = "invalid letters!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (preg_match("/[A-Za-z]+/", $value) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // alphanumeric
-   public function alphanumeric(string $field, $value, string $errmsg = "invalid alphanumeric!")
+   public function alphanumeric(string $field, string $value, string $errmsg = "invalid alphanumeric!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (preg_match("/[A-Z0-9a-z]+/", $value) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // alphanumenricwildcards | password
-   public function password(string $field, $value, string $errmsg = "invalid password!")
+   public function password(string $field, string $value, string $errmsg = "invalid password!")
    {
-      if (preg_match("/[A-Z0-9a-z$%&@#£=_\.]+/", $value) == false) {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
+      if ( preg_match_all("/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{\":;\'?\/>.<,])(?!.*\s).*$/", $value) == false ) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // email
    public function email(string $field, string $errmsg = "invalid email!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_EMAIL, [FILTER_FLAG_EMAIL_UNICODE]) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // telephone
-   public function telephone(string $field, $value, string $errmsg = "invalid telephone!")
+   public function telephone(string $field, string $value, string $errmsg = "invalid telephone!")
    {
-      if ( preg_match("/^[0-9-:\/WT]+$/", $value) == false ){
-         $this->errors[$field][] = $errmsg;
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
+      if ( preg_match("/^[0-9-+()]+$/", preg_replace("/\s/", "", $value)) == false ){
+         $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // ip
-   public function ip(string $field, $value, string $errmsg = "invalid ip address!")
+   public function ip(string $field, string $value, string $errmsg = "invalid ip address!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_IP, [FILTER_FLAG_IPV4, FILTER_FLAG_IPV6, FILTER_FLAG_NO_PRIV_RANGE, FILTER_FLAG_NO_RES_RANGE]) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // mac
-   public function mac(string $field, $value, string $errmsg = "invalid mac address!")
+   public function mac(string $field, string $value, string $errmsg = "invalid mac address!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_MAC) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // url
-   public function url(string $field, $value, string $errmsg = "invalid password!")
+   public function url(string $field, string $value, string $errmsg = "invalid url!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_URL, [FILTER_FLAG_SCHEME_REQUIRED, FILTER_FLAG_HOST_REQUIRED, FILTER_FLAG_PATH_REQUIRED, FILTER_FLAG_QUERY_REQUIRED]) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // domain
-   public function domain(string $field, $value, string $errmsg = "invalid password!")
+   public function domain(string $field, string $value, string $errmsg = "invalid domain!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if (filter_var($value, FILTER_VALIDATE_DOMAIN, [FILTER_FLAG_HOSTNAME]) == false) {
          $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // date
-   public function date()
+   public function date(string $field, string $value, string $errmsg = "invalid date!")
    {
+      $this->field = $field; $this->value = $value; $this->errmsg = $errmsg;
       if ( preg_match("/^[0-9-:\/WT]+$/", $value) == false ){
-         $this->errors[$field][] = $errMsg;
+         $this->errors[$field] = $errmsg;
       }
+      return $this;
    }
 
    // -----------
 
    // exact
+   public function exact(int $length)
+   {
+      if ( strlen($this->value) != $length ){
+         $this->errors[$this->field] = $this->errmsg;
+      }
+      return $this;
+   }
 
    // max
+   public function max(int $length)
+   {
+      if ( strlen($this->value) > $length ){
+         $this->errors[$this->field] = $this->errmsg;
+      }
+      return $this;
+   }
 
    // min
+   public function min(int $length)
+   {
+      if ( strlen($this->value) < $length ){
+         $this->errors[$this->field] = $this->errmsg;
+      }
+      return $this;
+   }
 
    // required
 
+   // ------------
+
+   // errors
+   public function errors()
+   {
+      return $this->errors;
+   }
+
 }
+
+// passthru('touch gamer.txt');
