7580229ebc59120c96a808baf5793ace5a012164
ebuka <ebukaodini@gmail.com>
2020-05-23 14:21:29 +0100

    added the user instance service

--- app/services/User.php
+++ app/services/User.php
@@ -4,47 +4,17 @@ namespace App\Services;
 class User
 {
 
-   private static $credentials;
+   public static $auth = false;
+   public static $username = '';
+   public static $role = '';
 
-   public function __construct(array $credentials)
+   public static function user()
    {
-
-      // make the credentials properties of the user
-      $credentials = self::objectify($credentials);
-
-      self::$credentials = $credentials;
-
-      // foreach ($credentials as $key => $value) {
-      //    self::$key = $value;
-      // }
-
-   }
-
-   public function user()
-   {
-      return self::$credentials;
-   }
-
-
-   // method for turning arrays to objects
-   private static function objectify(array $array)
-   {
-      if (is_array($array) && $array !== []) {
-         $obj = new \stdClass();
-         foreach ($array as $key => $value) {
-            // convert the value into an object if value is also an array
-            if (is_array($value)) {
-               $value = self::objectify($value);
-            }
-            // assign the value to the key
-            $obj->$key = $value;
-         }
-
-         $value = null;
-         return $obj;
-      }
-
-      return null;
+      return json_encode([
+         "auth"=>self::$auth,
+         "username"=>self::$username,
+         "role"=>self::$role,
+      ]);
    }
 
 }
