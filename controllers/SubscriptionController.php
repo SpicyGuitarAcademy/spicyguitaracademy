1b880aea13e5b9f335b824e34a52e756e4ab7e02
Ebuka Odini <ebukaodini@gmail.com>
2022-01-10 11:30:59 +0100

    fix: cleanup

--- controllers/SubscriptionController.php
+++ controllers/SubscriptionController.php
@@ -840,9 +840,3 @@ HTML;
       $res->success('Subscription plans', $plans);
    }
 }
-
-
-$date = date_create();
-date_add($date, date_interval_create_from_date_string('12 Months'));
-$end = date_format($date, "Y-m-d H:i:s");
-echo $end;
